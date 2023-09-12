780870a3d8e365db22b8821ea9ae1819c040d389
Ford2003 <tommyford28@googlemail.com>
2023-09-12 19:56:47 +0100

    make __init__.py empty and change packages in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='py-map-gen',
-    version='0.1',
+    version='0.10',
     author='Ford2003',
     author_email='tommyford28@gmail.com',
     description='A package for generating data for a world with a seed.',
-    packages=find_packages(),
-    requires=requirements,
+    long_description='',
+    packages=['py-map-gen'],
+    package_dir={'py-map-gen': 'py-map-gen'},
+    py_modules=['py-map-gen.funcs', 'py-map-gen.mersenne', 'py-map-gen.render'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independant"
+        "Operating System :: OS Independent"
     ]
 )
